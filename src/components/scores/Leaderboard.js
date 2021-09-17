0c407c1cf3cf82b3241203e5622ec58dc528774a
galmodovar <galmodovar1@gmail.com>
2021-09-17 12:50:51 -0500

    more work on leaderboard

--- src/components/scores/Leaderboard.js
+++ src/components/scores/Leaderboard.js
@@ -7,23 +7,26 @@ export const LeaderBoard = () => {
     const [currentUser, setUser] = useState()
     const [teamScores, setTeamScores] = useState([])
     const [userTeamScores, setUserTeamScores] = useState([])
+    const [localTeamScores, setLocalTeamScores] = useState([])
+    const [localTeamArr, setUserTeams] = useState([])
     const [myTeams, setMyTeams] = useState(new Map())
     const userId = localStorage.getItem("ncaaf_user")
-    
-     useEffect(
+
+    useEffect(
         () => {
             getMyTeams()
                 .then((data) => {
                     setTeams(data)
                 })
         },
-        [ userId ]
+        [userId]
     )
     useEffect(
         () => {
             getAllLocalTeams()
                 .then((data) => {
-                    setLocalTeams(data)
+                    let sortedTeams = data.sort((a, b) => a.userId - b.userId)
+                    setLocalTeams(sortedTeams)
                 })
         },
         []
@@ -35,7 +38,7 @@ export const LeaderBoard = () => {
                     setUser(user)
                 }
             })
-            },
+        },
         [localTeamsList]
     )
     useEffect(
@@ -50,66 +53,81 @@ export const LeaderBoard = () => {
     useEffect(() => {
         const events = teamScores?.events?.map(event => event.competitions[0].competitors)
         const myTeamScores = events?.filter((game) => {
-            for (const a of game ) {
+            for (const a of game) {
                 for (const myTeam of teamsList) {
                     if (parseInt(a.team.id) === myTeam.teamId) {
                         return a
-                      }  
-                    } 
-                    
+                    }
                 }
-            })
-            const scores = myTeamScores?.flat()
-            const userScores = scores?.filter((filterTeam => !!teamsList.some(myTeam => parseInt(filterTeam.id) === myTeam.teamId)))
-            setUserTeamScores(userScores)
-    
-            }, [teamScores] )
+
+            }
+        })
+        const scores = myTeamScores?.flat()
+        const userScores = scores?.filter((filterTeam => !!teamsList.some(myTeam => parseInt(filterTeam.id) === myTeam.teamId)))
+        setUserTeamScores(userScores)
+
+    }, [teamScores])
+
+    useEffect(() => {
+        const events = teamScores?.events?.map(event => event.competitions[0].competitors)
+        const myTeamScores = events?.filter((game) => {
+            for (const a of game) {
+                for (const localTeam of localTeamsList) {
+                    if (parseInt(a.team.id) === localTeam.teamId) {
+                        return a
+                    }
+                }
+
+            }
+        })
+        const scores = myTeamScores?.flat()
+        const localScores = scores?.filter((filterTeam => !!localTeamsList.some(myTeam => parseInt(filterTeam.id) === myTeam.teamId)))
+        setLocalTeamScores(localScores)
+
+    }, [teamScores])
 
 
-    // useEffect(() => {
-    //     const allTeams = teamsArr?.teams?.filter((filterTeam) => !!myTeams.some(myTeam => parseInt(filterTeam.team.id) === myTeam.teamId))
-    //     setUserTeams(allTeams)
-    // }, [myTeams] )
+    useEffect(() => {
+        const teamArr = Array.from(new Set(localTeamsList.map(a => a.userId))).map(id => {return localTeamsList.find(a => a.userId === id)
+ })
+        setUserTeams(teamArr)
+    }, [localTeamsList] )
 
     return (
         <>
-        <main className="mainContainer">
-            <section className="myTeamContainer">
-            <h2>{currentUser?.user.name}'s Score</h2>
-            {
-                userTeamScores?.map(
-                    (teamObject) => {
-                        return <p key={`team--${teamObject.team.id}`}>  <img src={teamObject.team.logo} className="teamsList"/> {teamObject.team.displayName} {teamObject.score}</p>
+            <main className="mainContainer">
+                <section className="myTeamContainer">
+                    <h2>{currentUser?.user.name}'s Score</h2>
+                    {
+                        userTeamScores?.map(
+                            (teamObject) => {
+                                return <p key={`team--${teamObject.team.id}`}>  <img src={teamObject.team.logo} className="teamsList" /> {teamObject.team.displayName} {teamObject.score}</p>
+                            }
+                        )
                     }
-                )
-            }
-            <h3>You have {teamsList.length} teams on your roster for the week.</h3>
-            </section>
-            <section className="myTeamContainer">
-            <h2>Current Scores</h2>
-            {
-                localTeamsList.map(
-                    (team) => {
-                        return <p key={`team--${team.id}`}> Week #{team.week}: User Name: {team.user.name} Team Score:{team.weeklyScore} </p>
+                    <h3>You have {teamsList.length} teams on your roster for the week.</h3>
+                </section>
+                <section className="localTeamContainer">
+                    <h2>Current Scores</h2>
+                    {
+                        localTeamScores?.map(
+                            (teamObject) => {
+                                    return <p key={`team--${teamObject.team.id}`}> <img src={teamObject.team.logo} className="teamsList" /> {teamObject.team.displayName} {teamObject.score} </p> 
+                            }
+                        )
                     }
-                )
-            }
-            <h3>You have {teamsList.length} teams on your roster for the week.</h3>
-            </section>
-            <section className="myTeamContainer">
-            <h2>LeaderBoard</h2>
-            {
-                localTeamsList.map(
-                    (team) => {
-                        return <p key={`team--${team.id}`}> Week #{team.week}: User Name: {team.user.name} Team Score:{team.weeklyScore} </p>
-                    }
-                )
-            }
-            <h3>You have {teamsList.length} teams on your roster for the week.</h3>
-            </section>
-        </main>
+                </section>
+                <section className="leaderContainer">
+                    <h2>LeaderBoard</h2>
+                            {
+                                localTeamArr?.map(team => {
+                                    return <p>{team.user.name}</p>
+                                    })   
+                            }
+                </section>
+            </main>
 
-            
+
         </>
     )
 }
