826829cafd8fa05916441d0d332a97fc85b1b1f4
galmodovar <galmodovar1@gmail.com>
2022-04-30 12:18:11 -0500

    edit: cleaning up headlines and rankings for main view

--- src/components/news/MainNews.js
+++ src/components/news/MainNews.js
@@ -28,31 +28,26 @@ export const MainNews = () => {
     )
     
     return (
-        <>      
-                <section className="newsContainer">
-                    <h3> Latest Headlines:</h3>
-                    {
-                        headlines.articles?.map(
-                        (headlineObject, i) => {
-                        return <p key={`headline--${i}`}> {headlineObject.headline} 
-                        <Link className="main-headline"
-                        to={{pathname: `${headlineObject.links.web.href}`,}} target="_blank">
-                        <span>Read more</span>
-                        </Link> </p>
-                        }
-                        )
-                    }
-                    <h3> Latest AP Poll Rankings:</h3>
-                    <div className="rankings">
-                    {
-                        rankings?.map(
-                        (rankingObject, i) => {
+        <>
+        <section className="newsContainer">
+            <h3> Latest Headlines:</h3>
+            {headlines.articles?.map((headlineObject, i) => {
+                return <p key={`headline--${i}`}> {headlineObject.headline}
+                <Link className="main-headline"
+                    to={{pathname: `${headlineObject.links.web.href}`,}} target="_blank">
+                    
+                    <span>Read more</span>
+                </Link>
+                    </p>
+                })}
+            
+            <h3> Latest AP Poll Rankings:</h3>
+                <div className="rankings">
+                    {rankings?.map((rankingObject, i) => {
                         return <p key={`ranking--${i}`} className="teamRankings"> #{rankingObject.current} <img src={rankingObject.team.logo} className="teamsList"/> {rankingObject.team.nickname} {rankingObject.recordSummary} </p>
-                        }
-                        )
-                    }
-                    </div>
-                </section>            
+                        })}
+                </div>
+        </section>            
         </>
-    )
-}
+        )
+    }
